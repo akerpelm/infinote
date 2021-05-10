1e186b7d683d61fc48315744b83aaaae9fec638d
akerpelm <akerpelm@u.rochester.edu>
2021-05-10 12:19:04 -0400

    begin backend auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namesake :api, defaults: { render: :json } do
+    resources :users, only: [:create, :new, :index, :show]
+    resource :session, only: [:create, :destroy, :show]
+  end
+  root to: 'static_pages#root'
 end
