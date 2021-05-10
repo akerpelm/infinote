b510b61a49a191b69af3b9ecdba97b98f5590faf
akerpelm <akerpelm@u.rochester.edu>
2021-05-10 12:01:31 -0400

    change db to psql, add semantic entry file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './frontend/entry.jsx',
+    entry: './frontend/infinote.jsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
